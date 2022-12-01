29475900373324c39fdda831a77e32dbd7144191
Mark Southern <marksouthern3@gmail.com>
2022-12-01 20:13:29 +0000

    Complete week 1

---
+++ data.R
@@ -0,0 +1,5 @@
+x <-
+structure(c(1L, 2L, 1L, 1L, 2L), levels = c("male", "female"), class = "factor")
+y <-
+structure(list(a = c(1, 2, 3, 4), b = c(2000, 800, 99, 4.2000000000000002
+)), class = "data.frame", row.names = c(NA, -4L))
---
+++ y.R
@@ -0,0 +1,2 @@
+structure(list(a = c(1, 2, 3, 4), b = c(2000, 800, 99, 4.2)), class = "data.frame", row.names = c(NA, 
+-4L))
